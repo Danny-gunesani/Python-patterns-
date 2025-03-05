af1a9faaee457a12c2e50e5596c8b601f0dbbc38
Danny <142622752+Danny-gunesani@users.noreply.github.com>
2025-03-05 14:19:11 +0500

    update

---
+++ patttern/pyramid.py
@@ -0,0 +1,9 @@
+num = 5
+for i in range(1 , num + 1):
+	for j in range(num - i):
+		print(" " , end=" ")
+	for k in range(1, 2*i):
+		print("*", end=' ')
+
+	print()
+	
---
+++ patttern/square.py
@@ -0,0 +1,6 @@
+num = 6
+
+for i in range(num) :
+    for j in range(num):
+        print('*', end = ' ')
+    print() 
